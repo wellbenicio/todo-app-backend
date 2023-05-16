e4aab558ca88c721cf1978ffa097270ec3ca7e47
Wellynton Benicio <wellynton.benicio@gmail.com>
2023-05-16 20:33:05 -0300

    Instaçao e configuraçao TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'ROOT',
+      database: 'todo',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
