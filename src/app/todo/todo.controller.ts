36a4dadd0bebcfc0ab8fa3d7b57e475f7860824c
Wellynton Benicio <wellynton.benicio@gmail.com>
2023-05-18 16:35:00 -0300

    Implementação todo.controller

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
 
 @Controller('api/v1/todos')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoSerice: TodoService) {}
+  @Get()
+  async index() {
+    return await this.todoSerice.findAll();
+  }
+  @Post()
+  async create(@Body() body) {
+    return await this.todoSerice.create(body);
+  }
+  @Get(':id')
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.todoSerice.findOneOrFail(id);
+  }
+  @Put(':id')
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+    return await this.todoSerice.update(id, body);
+  }
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.todoSerice.deleteById(id);
+  }
+}
